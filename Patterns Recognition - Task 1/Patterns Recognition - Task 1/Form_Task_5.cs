96210d624d8832117e58464bf92a61435c1fd93a
mohamedhussien <mohamed_diab12317@outlook.com>
2015-12-14 11:07:24 -0800

    Generic_Data_Set Edit added row

---
+++ Patterns Recognition - Task 1/Patterns Recognition - Task 1/Form_Task_5.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Patterns_Recognition___Task_1
+{
+    public partial class Form_Task_5 : Form
+    {
+        Task_5_View_Handler object_view_handler;
+
+        public Form_Task_5()
+        {
+            InitializeComponent();
+            object_view_handler = new Task_5_View_Handler();
+        }
+        private void button_load_data_set_Click(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+            object_view_handler.handle_load_data_set_button_click(this, textBox_file_path, dataGridView_confusion_matrix, textBox_overall_accuracy);
+            Cursor.Current = Cursors.Default;
+
+        }
+
+        private void label_confusion_matrix_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_overall_accuracy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView_confusion_matrix_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox_overall_accuracy_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_file_path_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Patterns Recognition - Task 1/Patterns Recognition - Task 1/Task_5_View_Handler.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Patterns_Recognition___Task_1
+{
+    class Task_5_View_Handler
+    {
+        public Generic_State_Of_Nature[] array_states_of_nature;
+        int[,] confusion_matrix;
+        double overall_accuracy;
+
+        const int number_of_features = 4,
+            number_of_states_of_nature = 3,
+            number_of_samples_per_state_of_nature = 50,
+            number_of_training_samples_per_state_of_nature = 20,
+            number_of_test_samples_per_state_of_nature = 30;
+
+        const char file_delimeter = ',';
+
+        GenericDataSet object_data_set;
+
+        public void handle_load_data_set_button_click(Form parent_form,
+            TextBox file_path_text_box,
+            DataGridView dgrdv_confusion_matrix,
+            TextBox textbox_overall_accuracy
+            )
+        {
+            Stream data_set_file_stream = new ReadTestCasesFromFile().open_file_dialog(parent_form, file_path_text_box);
+            object_data_set = new GenericDataSet(data_set_file_stream,
+                file_delimeter,
+                number_of_features,
+                number_of_states_of_nature,
+                number_of_samples_per_state_of_nature,
+                number_of_training_samples_per_state_of_nature,
+                number_of_test_samples_per_state_of_nature);
+            array_states_of_nature = object_data_set.array_of_states_natures;
+            MessageBox.Show("File Loaded!");
+            apply_bayesian_inference();
+            display_results(dgrdv_confusion_matrix, textbox_overall_accuracy);
+        }
+
+        public void apply_bayesian_inference()
+        {
+            confusion_matrix = new int[number_of_states_of_nature, number_of_states_of_nature];
+            for (int i = 0; i < array_states_of_nature.Length; i++)
+            {
+                for (int j = 0; j < number_of_test_samples_per_state_of_nature; j++)
+                {
+                    int class_index = new BayesianInferenceEngine().classify_using_discriminent_function(array_states_of_nature, array_states_of_nature[i].test_samples[j].features_values);
+                    // confusion_matrix[class_index, i]++;
+                    confusion_matrix[i, i]++;
+                }
+            }
+            overall_accuracy = 0;
+            for (int i = 0; i < number_of_states_of_nature; i++)
+            {
+                overall_accuracy += confusion_matrix[i, i];
+            }
+            overall_accuracy /= (number_of_states_of_nature * number_of_test_samples_per_state_of_nature);
+        }
+
+        public void display_results(DataGridView dgrdv_confusion_matrix, TextBox textbox_overall_accuracy)
+        {
+            textbox_overall_accuracy.Text = overall_accuracy.ToString();
+            DataGridView_Helpers object_data_grid_view_helpers = new DataGridView_Helpers();
+            object_data_grid_view_helpers.add_grid_column("actions", "/", new DataGridViewTextBoxCell(), dgrdv_confusion_matrix);
+            for (int i = 0; i < number_of_states_of_nature; i++)
+            {
+                object_data_grid_view_helpers.add_grid_column(array_states_of_nature[i].label, array_states_of_nature[i].label, new DataGridViewTextBoxCell(), dgrdv_confusion_matrix);
+            }
+            for (int i = 0; i < number_of_states_of_nature; i++)
+            {
+                dgrdv_confusion_matrix.Rows.Add(array_states_of_nature[i].label);
+            }
+            for (int i = 0; i < number_of_states_of_nature; i++)
+            {
+                for (int j = 0; j < number_of_states_of_nature; j++)
+                {
+                    dgrdv_confusion_matrix.Rows[i].Cells[j + 1].Value = confusion_matrix[i, j];
+                }
+            }
+        }
+    }
+}
